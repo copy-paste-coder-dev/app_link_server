7440611eff3912459e15f07fbb9cc49e73a3a816
copy_paste_coder <shajith.atmega@gmail.com>
2025-01-27 15:55:35 +0530

    changes

--- server.js
+++ server.js
@@ -1,18 +1,31 @@
+// const express = require("express");
+// const path = require("path");
+
+// const app = express();
+// const PORT = process.env.PORT || 3000;
+
+// // Serve the .well-known directory
+// app.use("/.well-known", express.static(path.join(__dirname, ".well-known")));
+
+// // Root route for testing
+// app.get("/", (req, res) => {
+//   res.send("Welcome to the Android App Links Server!");
+// });
+
+// // Start the server
+// app.listen(PORT, () => {
+//   console.log(`Server is running at http://localhost:${PORT}`);
+// });
+//app.js
+
 const express = require("express");
-const path = require("path");
-
+const serverless = require("serverless-http");
 const app = express();
-const PORT = process.env.PORT || 3000;
+const router = express.Router();
 
-// Serve the .well-known directory
-app.use("/.well-known", express.static(path.join(__dirname, ".well-known")));
-
-// Root route for testing
-app.get("/", (req, res) => {
-  res.send("Welcome to the Android App Links Server!");
+router.get("/", (req, res) => {
+    res.send("App is running..");
 });
 
-// Start the server
-app.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}`);
-});
+app.use("/.netlify/functions/app", router);
+module.exports.handler = serverless(app);
